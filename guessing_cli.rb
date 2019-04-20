b46352b6b9d9f1df30f71a9fba5daca98a50e9e2
Henry Koehler <henryk1229@gmail.com>
2019-04-20 22:51:22 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -2,7 +2,7 @@
 def run_guessing_game
   puts "Guess a number between 1 and 6."
   user_guess = gets.chomp 
-  computer_guess = prng.rand(6)
+  computer_guess = random.new(6)
   if computer_guess == user_guess
     puts "You guessed the correct number!"
   elsif computer_guess != user_guess
